4f07e54d96758b63174b86996974c220ae157386
Allan <allan_hc@outlook.com>
2025-08-28 10:04:24 -0600

    limpieza de imagenes inncesarias y cambio de logo y favicon

--- src/components/team-switcher.tsx
+++ src/components/team-switcher.tsx
@@ -4,6 +4,7 @@ import * as React from "react"
 import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
 import Link from "next/link"
+import Image from "next/image"
 
 export function TeamSwitcher({ teams }: {
   teams: {
@@ -27,12 +28,13 @@ export function TeamSwitcher({ teams }: {
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Link href="/" className="flex items-center gap-3 flex-1">
+              <Link href="/admin" className="flex items-center gap-3 flex-1">
                 <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <activeTeam.logo className="size-4" />
+                  {/* <activeTeam.logo className="size-4" /> */}
+                  <Image src="/logo.png" alt="Logo" style={{ objectFit: "contain", objectPosition: "left" }} fill priority sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
                 </div>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{activeTeam.name}</span>
+                <div className="grid flex-1 text-center text-lg leading-tight">
+                  <span className="truncate font-bold text-lg">{activeTeam.name}</span>
                 </div>
               </Link>
             </SidebarMenuButton>
